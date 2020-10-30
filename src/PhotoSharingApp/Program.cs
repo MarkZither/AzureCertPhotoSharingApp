7633e39516d8b297e137daece1c0a35cb3db8e0f
MarkZither <mark.burton@zither-it.co.uk>
2020-10-30 22:00:34 +0100

    Initial Commit

---
+++ src/PhotoSharingApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static async Task Main(string[] args)
+        {
+                        var builder = new ConfigurationBuilder();
+            // tell the builder to look for the appsettings.json file
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<Program>();
+
+            Configuration = builder.Build();
+
+            var connectionString = Configuration["StorageAccountConnectionString"];
+
+            if (!CloudStorageAccount.TryParse(connectionString,
+                out CloudStorageAccount storageAccount))
+            {
+                Console.WriteLine("Unable to parse connection string");
+                return;
+            }
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("photoblobs");
+            bool created = await blobContainer.CreateIfNotExistsAsync();
+
+            Console.WriteLine(created ? "Created the Blob container" : "Blob container already exists.");
+        }
+    }
+}
